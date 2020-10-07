f389ec2e9d5e3245ccaf6a5ce1b100f913068b56
flytosky-f <42865761+flytosky-f@users.noreply.github.com>
2020-10-07 20:28:13 +0800

    Monkey's audio SDK V5.54

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MAC
- Monkey's Audio SDK for Windows command line.
+Monkey's Audio SDK for Windows command line.
+
+Port from [Monkey's Audio](https://monkeysaudio.com/developers.html)
---
+++ Source/Shared/Version.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// major version number
+#define MAC_VERSION_MAJOR 5
+
+// build version number
+#define MAC_VERSION_REVISION 54
+
+// Inno setup
+#define INNO_VERSION 554
+#define INNO_NAME "5.54"
